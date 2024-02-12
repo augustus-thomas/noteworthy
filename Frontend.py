b2e78d26bbe82fa2e90cc0054bd458a86ce1f0a9
Chrisvann1 <Christophervann123@gmail.com>
2024-02-12 12:01:18 -0500

    Frontend init

---
+++ Frontend.py
@@ -0,0 +1,6 @@
+class Frontend: 
+    def __init__(self, retrieve, add, search, view): 
+        self.retrieve = retrieve
+        self.add = add 
+        self.search = search 
+        self.view = view
