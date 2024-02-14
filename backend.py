1a0283b9ecb1932bbd3e3210b5ddd4a35dac6d47
Gus Thomas <augustusrthomas@gmail.com>
2024-02-14 11:29:53 -0500

    my backend briefly

---
+++ backend.py
@@ -0,0 +1,13 @@
+# functions for backend calls
+
+# name is a filename. can have .md extension passed
+# if it has a period a different extension we will force it to be .md
+
+# contents are string, filename may or may not already exist
+def write(contents, filename):
+	pass
+
+# if filename doesn't exist, return a string with error message
+# if filename exists, return a string with its contents
+def view(filename):
+	pass
