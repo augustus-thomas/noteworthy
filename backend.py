72e7db2b21858714dda7d484e7f7842f93c88372
Gus Thomas <augustusrthomas@gmail.com>
2024-02-15 08:10:07 -0500

    Changes to functional backend

--- backend.py
+++ backend.py
@@ -1,9 +1,17 @@
 # functions for backend calls
 
+
+
+
+# return whether filename exists
+def search_for_file(filename):
+	pass
+
 # name is a filename. can have .md extension passed
 # if it has a period a different extension we will force it to be .md
 
 # contents are string, filename may or may not already exist
+# filename is optional. if none, first word in contents dot md
 def write(contents, filename):
 	pass
 
@@ -11,3 +19,4 @@ def write(contents, filename):
 # if filename exists, return a string with its contents
 def view(filename):
 	pass
+
