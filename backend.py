b9689157c61f210d986c1f077f081c3acfd9e214
Gus Thomas <augustusrthomas@gmail.com>
2024-02-16 10:52:12 -0500

    Search by filename and search by date backend

--- backend.py
+++ backend.py
@@ -1,10 +1,24 @@
 # functions for backend calls
 import os
 import time
+import datetime
 
 # return whether filename exists
 def search_for_file(filename):
-	pass
+	return filename in os.listdir("Notes")
+
+# return files modified by date. default to after passed date
+def search_by_date(filename, date, after=True):
+	year_date_month = date.split('\\')
+	# make date into EPOCH
+	epoch = datetime.datetime(*year_date_month, 0, 0, 0).strftime('%s')
+	output = ""
+	for filename in os.listdir("Notes"):
+		# get the time file was last modified
+		modified = os.path.getmtime(filename)
+		if modified >= epoch:
+			output.append(filename + '\n')
+	return output
 
 # name is a filename. can have .md extension passed
 # if it has a period a different extension we will force it to be .md
