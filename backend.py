4ec1da6aef90eb81a10aafa22acf46c43d9c7162
Gus Thomas <augustusrthomas@gmail.com>
2024-02-19 08:19:42 -0500

    backend write & test file output ignored

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 # functions for backend calls
 import os
 import time
-import datetime
+from datetime import date
 
 # return whether filename exists
 def search_for_filename(filename):
@@ -30,15 +30,30 @@ def search_for_modfication_on_date(filename, date):
 
 # contents are string, filename may or may not already exist
 # filename is optional. if none, first word in contents dot md
-def write(contents, filename, data):
-	pass
+def write(contents, date=date.today(), filename=None):
+	# contents should be a string
+	assert type(contents) == type("string")
+
+	# find the filename if none was passed
+	if filename == None:
+		potential_filename = contents.split(" ", 1)[0]
+		filename = potential_filename.split(",", 1)[0]
+
+	edit_char = "w"
+	if search_for_filename(filename):
+		# append if filename already exists
+		edit_char = "a"
+
+	with open(filename, edit_char) as f:
+		f.write('\u2001' + str(date) + '\u2001' + '\n')
+		f.write(contents)
 
 # if filename doesn't exist, return a string with error message
 # if filename exists, return a string with its contents
 def view(filename):
 	if search_for_filename(filename):
 		with open(filename) as f:
-    		s = f.read()
+			s = f.read()
 	else:
 		s = "File does not exist"
 	return s
